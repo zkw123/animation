135f378351ac02314704c980d9b0f130ad0d0001
zkw123 <906269996@qq.com>
2020-11-07 19:05:25 +0800

    test

---
+++ door_animation/Assets/Scripts/MoveMonster.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMonster : MonoBehaviour
+{
+    public Transform kPlayer;
+    public float fDistance = 5f;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float distance = Mathf.Sqrt(h * h + v * v);
+        float speed = distance / Time.deltaTime;
+        animator.SetFloat("MoveSpeed", speed);
+
+        bool fire = Input.GetButtonDown("Fire1");
+        if (fire)
+        {
+            animator.SetTrigger("Die");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 kNewPos = Camera.main.transform.position + fDistance * Camera.main.transform.forward;
+        kNewPos.y = 0;
+
+        kPlayer.position = kNewPos;
+    }
+}
---
+++ door_animation/Assets/Scripts/hit.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngineInternal;
+
+public class hit : MonoBehaviour
+{
+    public bool collider = false;
+    private Vector3 deltaPos = Vector3.zero;
+    public Transform camera;
+    public Animator a;
+    private Vector3 pos;
+    int i = 0;
+    int j = 0;
+    public GameObject particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+     void Update()
+    {
+        if(collider&& a.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            camera.localPosition -= deltaPos;
+
+            deltaPos = Random.insideUnitSphere / 3.0f;
+
+            camera.localPosition += deltaPos;
+            i++;
+            j++;
+
+        }
+        if (i == 50)
+        {
+            collider = false;
+            i = 0;
+        }
+        if(j==500)
+        {
+            particle.SetActive(false);
+            j = 0;
+        }
+    }
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        pos = contact.point;
+        //pos = transform.position;
+        collider = true;
+        particle.SetActive(true);
+        particle.transform.position = new Vector3(pos.x,pos.y,pos.z);
+        particle.transform.forward = collision.transform.forward;
+    }
+}
